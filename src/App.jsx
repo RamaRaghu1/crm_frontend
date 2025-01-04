4c9dd2878bd1c592f3341436b4b7e9af3407b06d
Rama Raghu <ramanadar003@gmail.com>
2025-01-04 12:47:01 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,28 @@ import { Custom } from "./main";
 import { Navigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
-const tokenexpired = () => {
+const isAuthenticated = () => {
   let token = localStorage.getItem("accessToken");
-if(token===null || token===undefined){
-  return false;
-}
+
+  console.log("token_________", token);
+  if (token === null || token === undefined) {
+    return false;
+  }
   let decodedToken = jwtDecode(token);
   let currentDate = new Date();
-console.log("_____",decodedToken.exp * 1000 < currentDate.getTime() ? false : true)
+  console.log(
+    "_____",
+    decodedToken.exp * 1000 < currentDate.getTime() ? false : true
+  );
+  console.log("decodedToken.exp____", decodedToken.exp)
   return decodedToken.exp * 1000 < currentDate.getTime() ? false : true;
 };
+
 
 function App() {
   return (
     <>
       <Custom>
-       
         <Outlet />
       </Custom>
     </>
@@ -29,7 +35,7 @@ function App() {
 }
 
 export const ProtectedRoute = () => {
-  return tokenexpired() ? <App /> : <Navigate to={"/login"} />;
+  return isAuthenticated() ? <App /> : <Navigate to={"/login"} />;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,34 @@ import "./index.css";
 import store from "./redux/store.js";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-
+import toast from "react-hot-toast";
 import { Toaster } from "react-hot-toast";
 
 import { useLoadUserQuery } from "./redux/features/api/apiSlice.js";
 import { router } from "./router.jsx";
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 export const Custom = ({ children }) => {
-  const { isLoading } = useLoadUserQuery({});
+
+
+const navigate=useNavigate();
+
+
+  const { isLoading, error, data } = useLoadUserQuery({});
+
+useEffect(()=>{
+  if(error && !data.success){
+    if (error) {
+      const errorMessage = error;
+      toast.error(errorMessage.message)
+     navigate("/login")
+  }
+  }
+},[error, data])
+
+
   return <>{isLoading ? <h1>Loading....</h1> : <>{children}</>}</>;
 };
 
